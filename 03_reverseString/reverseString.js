a2695519b59ee06353619603cfbdd53410446c35
ohnix <65321469+ohnix@users.noreply.github.com>
2023-03-19 20:31:57 -0400

    solved the third one quite organically

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  return str.split("").reverse().join("");
 };
+
+console.log(reverseString("hello there 123! abc!"));
 
 // Do not edit below this line
 module.exports = reverseString;
