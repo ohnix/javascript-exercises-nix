9d050c4dddd0306e57874b5defe49e761192acb7
ohnix <65321469+ohnix@users.noreply.github.com>
2023-03-27 17:27:30 -0400

    doesn't pass test 3

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
+
+console.log(sumAll(123, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
