efc04e8f3cdb296b1f7df7c7a79d150d1af5a71c
ohnix <65321469+ohnix@users.noreply.github.com>
2023-03-30 23:03:57 -0400

    progress on leapYear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 == 0 && year % 400 == 0) {
+    console.log("Yep! Leap year!");
+  }
 };
+
+console.log(leapYears(1996));
 
 // Do not edit below this line
 module.exports = leapYears;
+
+//if year is divisble by 4 = true &&
+//if year is divisible by 400 = true
+//else if
+//if year divisible by 100 = false
