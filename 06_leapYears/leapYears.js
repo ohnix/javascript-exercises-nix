90c2a097af254ac790dcf0dab1790e6c0be8b8cc
ohnix <65321469+ohnix@users.noreply.github.com>
2023-05-18 19:14:57 -0400

    solved leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,6 @@
 const leapYears = function (year) {
-  if (year % 4 == 0 && year % 400 == 0) {
-    console.log("Yep! Leap year!");
-  }
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
-
-console.log(leapYears(1996));
 
 // Do not edit below this line
 module.exports = leapYears;
