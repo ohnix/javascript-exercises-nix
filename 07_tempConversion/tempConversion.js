0ef43653e4f38fb584fb8a54f96d809cbb8017b6
ohnix <65321469+ohnix@users.noreply.github.com>
2023-05-21 17:34:26 -0400

    windows WSL commit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function () {
+  let farenheit = 32;
+  let oldCelsius = (farenheit - 32) * (5 / 9);
+  let celsius = Math.round(oldCelsius * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function () {
+  let celsius = 0;
+  let oldFarenheit = (celsius * 9) / 5 + 32;
+  let farenheit = Math.round(oldFarenheit * 10) / 10;
+  return farenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
