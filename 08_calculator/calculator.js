069a5999bc15b52b21162488e0bef59fa1abfc12
ohnix <65321469+ohnix@users.noreply.github.com>
2023-12-31 20:39:02 -0500

    update

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,22 @@
-const add = function() {
-	
+const add = function (a, b) {
+  let x = a + b;
+  console.log(x);
+  return x;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  let x = a - b;
+  console.log(x);
+  return x;
 };
 
-const sum = function() {
-	
-};
+const sum = function () {};
 
-const multiply = function() {
+const multiply = function () {};
 
-};
+const power = function () {};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +25,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
