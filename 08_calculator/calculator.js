e74480c563db57afd0ac19d3f824d92b3411d243
ohnix <65321469+ohnix@users.noreply.github.com>
2024-01-18 00:01:57 -0500

    did calc

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,22 +1,46 @@
 const add = function (a, b) {
-  let x = a + b;
-  console.log(x);
-  return x;
+  // let x = a + b;
+  // console.log(x);
+  // return x;
+  return a + b;
 };
 
 const subtract = function (a, b) {
-  let x = a - b;
-  console.log(x);
-  return x;
+  // let x = a - b;
+  // console.log(x);
+  // return x;
+  return a - b;
 };
 
-const sum = function () {};
+const sum = function (array) {
+  let result = array.reduce(
+    (firstValue, lastValue) => firstValue + lastValue,
+    0,
+  );
+  return result;
+};
 
-const multiply = function () {};
+const multiply = function (array) {
+  let result = array.reduce((firstValue, lastValue) => firstValue * lastValue);
+  return result;
+};
 
-const power = function () {};
+const power = function (a, b) {
+  let result = Math.pow(a, b);
+  return result;
+};
 
-const factorial = function () {};
+const factorial = function (n) {
+  if (n === 0 || n === 1) {
+    return 1; // Factorial of 0 and 1 is 1
+  } else {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
+};
 
 // Do not edit below this line
 module.exports = {
