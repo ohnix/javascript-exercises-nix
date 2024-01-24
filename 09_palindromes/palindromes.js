d255c758cadad360a7029f02289661267adbd096
ohnix <65321469+ohnix@users.noreply.github.com>
2024-01-23 21:21:21 -0500

    imperfect method here

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let processedString = string.split("").reverse().join("").toLowerCase();
+  return processedString === string;
 };
 
 // Do not edit below this line
